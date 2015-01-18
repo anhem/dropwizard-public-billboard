09209ba0c438e2542926cba4ddfea69b109e871f
Andreas Hemmingsson <andhem@gmail.com>
2015-01-18 16:19:45 +0100

    cleanup

--- billboard-core/src/main/java/com/example/billboard/db/binder/MessageBinder.java
+++ billboard-core/src/main/java/com/example/billboard/db/binder/MessageBinder.java
@@ -8,28 +8,16 @@ import org.skife.jdbi.v2.sqlobject.BindingAnnotation;
 
 import java.lang.annotation.*;
 
-/**
- * Created with IntelliJ IDEA.
- * User: andreas
- * Date: 2015-01-18
- * Time: 15:16
- * To change this template use File | Settings | File Templates.
- */
 @BindingAnnotation(MessageBinder.MessageBinderFactory.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.PARAMETER})
 
-public @interface MessageBinder
-{
+public @interface MessageBinder {
 
-    public static class MessageBinderFactory implements BinderFactory
-    {
-        public Binder build(Annotation annotation)
-        {
-            return new Binder<MessageBinder, Message>()
-            {
-                public void bind(SQLStatement q, MessageBinder bind, Message arg)
-                {
+    public static class MessageBinderFactory implements BinderFactory {
+        public Binder build(Annotation annotation) {
+            return new Binder<MessageBinder, Message>() {
+                public void bind(SQLStatement q, MessageBinder bind, Message arg) {
                     q.bind("ident", arg.getId());
                     q.bind("nom", arg.getName());
                 }
