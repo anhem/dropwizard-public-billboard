3914fc6134aec393e7c9d5817488603feb63aa9f
Andreas Hemmingsson <andhem@gmail.com>
2015-01-18 15:25:19 +0100

    added sql queries

--- billboard-core/src/main/java/com/example/billboard/db/MessageDao.java
+++ billboard-core/src/main/java/com/example/billboard/db/MessageDao.java
@@ -1,4 +1,20 @@
 package com.example.billboard.db;
 
-public class MessageDao {
+import com.example.billboard.core.model.Message;
+import com.example.billboard.db.mapper.MessageMapper;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
+
+import java.util.List;
+
+@RegisterMapper(MessageMapper.class)
+public interface MessageDao {
+
+    @SqlUpdate("insert into message (created, subject, text, name) values (:created, :subject, :text, :name)")
+    void insert(@BindBean Message message);
+
+    @SqlQuery("select * from message order by created desc")
+    List<Message> getAll();
 }
